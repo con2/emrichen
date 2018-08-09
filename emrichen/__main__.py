a39d17b053538f9b46d45638f5d27da1e43f03db
Aarni Koskela <akx@iki.fi>
2018-08-09 12:47:01 +0300

    Make CLI testable (parser must be wrapped in a function so capsys can be used)

--- emrichen/__main__.py
+++ emrichen/__main__.py
@@ -7,68 +7,70 @@ from .template import Template
 from .input import PARSERS
 from .output import RENDERERS
 
-parser = argparse.ArgumentParser(
-    description='A YAML to YAML preprocessor.',
-    prog='emrichen',
-    epilog='Variable precedence: -D > -e > -f',
-)
-parser.add_argument(
-    'template_file',
-    nargs='?',
-    type=argparse.FileType('r'),
-    default=sys.stdin,
-    help='The YAML template to process. If unspecified, the template is read from stdin.',
-)
-
-parser.add_argument(
-    '--template-format',
-    choices=PARSERS,
-    default=None,
-    help=(
-        'Template format. If unspecified, attempted to be autodetected from the '
-        'input filename (but defaults to YAML).'
-    ),
-)
-parser.add_argument(
-    '--var-file', '-f',
-    dest='var_files',
-    metavar='VAR_FILE',
-    type=argparse.FileType('r'),
-    action='append',
-    default=[],
-    help=(
-        'A YAML file containing an object whose top-level keys will be defined as variables. '
-        'May be specified multiple times.'
+def get_parser():
+    parser = argparse.ArgumentParser(
+        description='A YAML to YAML preprocessor.',
+        prog='emrichen',
+        epilog='Variable precedence: -D > -e > -f',
     )
-)
-parser.add_argument(
-    '--define', '-D',
-    metavar='VAR=VALUE',
-    action='append',
-    default=[],
-    help='Defines a single variable. May be specified multiple times.',
-)
-parser.add_argument(
-    '--output-file', '-o',
-    type=argparse.FileType('w'),
-    default=sys.stdout,
-    help='Output file. If unspecified, the template output is written into stdout.',
-)
-parser.add_argument(
-    '--output-format',
-    choices=RENDERERS,
-    default=None,
-    help=(
-        'Output format. If unspecified, attempted to be autodetected from the '
-        'output filename (but defaults to YAML).'
-    ),
-)
-parser.add_argument(
-    '--include-env', '-e',
-    action='store_true',
-    default=False,
-    help='Expose process environment variables to the template.',
-)
+    parser.add_argument(
+        'template_file',
+        nargs='?',
+        type=argparse.FileType('r'),
+        default=sys.stdin,
+        help='The YAML template to process. If unspecified, the template is read from stdin.',
+    )
+
+    parser.add_argument(
+        '--template-format',
+        choices=PARSERS,
+        default=None,
+        help=(
+            'Template format. If unspecified, attempted to be autodetected from the '
+            'input filename (but defaults to YAML).'
+        ),
+    )
+    parser.add_argument(
+        '--var-file', '-f',
+        dest='var_files',
+        metavar='VAR_FILE',
+        type=argparse.FileType('r'),
+        action='append',
+        default=[],
+        help=(
+            'A YAML file containing an object whose top-level keys will be defined as variables. '
+            'May be specified multiple times.'
+        )
+    )
+    parser.add_argument(
+        '--define', '-D',
+        metavar='VAR=VALUE',
+        action='append',
+        default=[],
+        help='Defines a single variable. May be specified multiple times.',
+    )
+    parser.add_argument(
+        '--output-file', '-o',
+        type=argparse.FileType('w'),
+        default=sys.stdout,
+        help='Output file. If unspecified, the template output is written into stdout.',
+    )
+    parser.add_argument(
+        '--output-format',
+        choices=RENDERERS,
+        default=None,
+        help=(
+            'Output format. If unspecified, attempted to be autodetected from the '
+            'output filename (but defaults to YAML).'
+        ),
+    )
+    parser.add_argument(
+        '--include-env', '-e',
+        action='store_true',
+        default=False,
+        help='Expose process environment variables to the template.',
+    )
+    return parser
 
 
 def determine_format(filelike, choices, default):
@@ -79,8 +81,11 @@ def determine_format(filelike, choices, default):
     return default
 
 
-def main():
-    args = parser.parse_args()
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+    parser = get_parser()
+    args = parser.parse_args(args)
 
     override_variables = dict(item.split('=', 1) for item in args.define)
 
