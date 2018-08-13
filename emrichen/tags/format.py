170d4cbaf76a6231511740f00e67092f9ee5fe7e
Aarni Koskela <akx@iki.fi>
2018-08-13 15:34:21 +0300

    Support JSONPath in '!Format'
    Fixes #8

--- emrichen/tags/format.py
+++ emrichen/tags/format.py
@@ -1,6 +1,28 @@
+import string
+
 from .base import BaseTag
+from .lookup import find_jsonpath_in_context
+
+
+class JSONPathFormatter(string.Formatter):
+    def __init__(self, tag, context):
+        super().__init__()
+        self.tag = tag
+        self.context = context
+
+    def get_field(self, field_name, args, kwargs):
+        return (self._get_in_context(field_name), 0)
+
+    def _get_in_context(self, selector):
+        matches = find_jsonpath_in_context(selector, self.context)
+        if matches:
+            return matches[0].value
+        else:
+            if selector in self.context:  # direct dotted key in context
+                return self.context[selector]
+        raise KeyError(f'{self.tag}: {selector} not found in context')
 
 
 class Format(BaseTag):
     def enrich(self, context):
-        return self.data.format_map(context)
+        return JSONPathFormatter(tag=self, context=context).format(self.data)
