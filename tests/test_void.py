bb4005c441069c277b8099534da32836fce32c4c
Santtu Pajukanta <santtu@pajukanta.fi>
2018-08-13 23:39:37 +0300

    Void: add a new test illustrating !Void at top level erasing the whole document

--- tests/test_void.py
+++ tests/test_void.py
@@ -1,8 +1,9 @@
 import json
 
-from emrichen import Template
+from emrichen import Template, emrichen
 
-template = Template.parse("""
+
+TEMPLATE = Template.parse("""
 Smile: !Void
 Sweet: !Void
 Sister: !Void
@@ -13,7 +14,12 @@ Service: !Void
 
 
 def test_void_yaml():
-    assert template.render({}, 'yaml').strip() == 'Surprise: true'
+    assert TEMPLATE.render({}, 'yaml').strip() == 'Surprise: true'
+
 
 def test_void_json():
-    assert json.loads(template.render({}, 'json')) == [{"Surprise": True}]
+    assert json.loads(TEMPLATE.render({}, 'json')) == [{"Surprise": True}]
+
+
+def test_void_top_level():
+    assert emrichen('!Void\n---!Void\n---\na: 5').strip() == 'a: 5'
