481c325b3dbc0c402e1f999f87f8eca0ccdc1831
Santtu Pajukanta <santtu@pajukanta.fi>
2018-09-28 12:28:23 +0300

    output: fix json output incurring an extra list around the document

--- emrichen/output.py
+++ emrichen/output.py
@@ -3,9 +3,23 @@ import json
 import pyaml
 import yaml
 
+
+def render_json(data):
+    if not isinstance(data, list) or len(data) != 1:
+        raise TypeError(
+            "JSON output can only handle a single document. "
+            "A common cause for this error is trying to render a multi-document "
+            "YAML template into JSON. If you use a YAML template with JSON output, "
+            "it may only contain a single document. !Defaults documents do not count "
+            "as they are stripped from the output."
+        )
+
+    return json.dumps(data[0], ensure_ascii=False, indent=2)
+
+
 RENDERERS = {
     'yaml': lambda data: yaml.dump_all(data, Dumper=pyaml.PrettyYAMLDumper, default_flow_style=False),
-    'json': lambda data: json.dumps(data, ensure_ascii=False, indent=2),
+    'json': render_json,
 }
 
 
