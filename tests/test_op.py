1855a916b67056d686069dd46b0906c8de2e1b61
Santtu Pajukanta <santtu@pajukanta.fi>
2018-10-22 10:22:40 +0300

    Op: add a test illustrating the list form

--- tests/test_op.py
+++ tests/test_op.py
@@ -9,3 +9,8 @@ def test_op():
   b: 1
 ''', 'yaml')
     assert template.enrich({}) == [2]
+
+
+def test_op_list():
+    template = Template.parse('!Op [1, +, 1]')
+    assert template.enrich({}) == [2]
