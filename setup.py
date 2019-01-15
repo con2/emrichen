22dbecb03c5b7fd009504cd1dae215e4fcfa9dee
Santtu Pajukanta <santtu@pajukanta.fi>
2019-01-15 21:29:18 +0200

    fix metadata, release 0.1.1

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python
 
+import os
 from setuptools import find_packages, setup
+
+
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='emrichen',
-    version='0.1.0',
+    version='0.1.1',
     description='Template engine for YAML & JSON',
-    long_description='',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     license='MIT',
     author='Santtu Pajukanta',
     author_email='santtu@pajukanta.fi',
-    url='http://github.com/japsu/emrichen',
+    url='http://github.com/con2/emrichen',
     packages = find_packages(exclude=["tests"]),
     zip_safe=True,
     entry_points={
